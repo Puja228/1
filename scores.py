9311f71836c9a736a85561e02c920613dfc07180
Puja228 <71925640+Puja228@users.noreply.github.com>
2020-09-26 15:47:56 +0530

    Add files via upload

---
+++ scores.py
@@ -0,0 +1,21 @@
+l = []
+second_lowest_names = []
+scores = set()
+
+for _ in range(int(input())):
+    name = input()
+    score = float(input())
+    l.append([name, score])
+    scores.add(score)
+print(l)
+print(scores)
+
+second_lowest = sorted(scores)[1]
+print(second_lowest)
+
+for name, score in l:
+    if score == second_lowest:
+        second_lowest_names.append(name)
+
+for name in sorted(second_lowest_names):
+    print(name, end='\n')
